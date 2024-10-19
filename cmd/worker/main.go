7be4bd6cb7501c7f304e2e9ce91f840af2441089
jaxron <126792083+jaxron@users.noreply.github.com>
2024-10-19 16:31:31 +0800

    fix(worker): correct flag logic

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -30,22 +30,20 @@ func main() {
 
 // newRootCmd creates a new root command.
 func newRootCmd() *cobra.Command {
-	var workerCount int
-
 	rootCmd := &cobra.Command{
 		Use:   "worker",
 		Short: "Start the rotector worker",
 		Long:  `This command starts the rotector worker, which can be either a group worker or a user worker.`,
 	}
-	rootCmd.PersistentFlags().IntVarP(&workerCount, "workers", "w", 1, "Number of workers to start")
-	rootCmd.AddCommand(newUserCmd(workerCount))
-	rootCmd.AddCommand(newGroupCmd(workerCount))
+	rootCmd.PersistentFlags().IntP("workers", "w", 1, "Number of workers to start")
+	rootCmd.AddCommand(newUserCmd())
+	rootCmd.AddCommand(newGroupCmd())
 
 	return rootCmd
 }
 
 // newUserCmd creates a new user worker command.
-func newUserCmd(workerCount int) *cobra.Command {
+func newUserCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   UserWorker,
 		Short: "Start user workers",
@@ -56,15 +54,17 @@ func newUserCmd(workerCount int) *cobra.Command {
 		&cobra.Command{
 			Use:   UserWorkerTypeFriend,
 			Short: "Start friend workers",
-			Run: func(_ *cobra.Command, _ []string) {
-				runWorkers(UserWorker, UserWorkerTypeFriend, workerCount)
+			Run: func(cmd *cobra.Command, _ []string) {
+				count, _ := cmd.Flags().GetInt("workers")
+				runWorkers(UserWorker, UserWorkerTypeFriend, count)
 			},
 		},
 		&cobra.Command{
 			Use:   UserWorkerTypeGroup,
 			Short: "Start group workers",
-			Run: func(_ *cobra.Command, _ []string) {
-				runWorkers(UserWorker, UserWorkerTypeGroup, workerCount)
+			Run: func(cmd *cobra.Command, _ []string) {
+				count, _ := cmd.Flags().GetInt("workers")
+				runWorkers(UserWorker, UserWorkerTypeGroup, count)
 			},
 		},
 	)
@@ -73,7 +73,7 @@ func newUserCmd(workerCount int) *cobra.Command {
 }
 
 // newGroupCmd creates a new group worker command.
-func newGroupCmd(_ int) *cobra.Command {
+func newGroupCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   GroupWorker,
 		Short: "Start group workers",
